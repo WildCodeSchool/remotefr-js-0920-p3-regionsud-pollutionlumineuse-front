b5062e67874b33fc1d9f96dbe797a5dae84e9258
CASTAN <christophecastan4@gmail.com>
2020-12-14 16:14:24 +0100

    début header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,23 @@
-export default function Header() {
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+ class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+    };
+  }
+
+  handleClick = () => {
+    const {clicked} = this.state;
+  }
+  
+  
+  
+  
+  
+  
   return <></>;
 }
