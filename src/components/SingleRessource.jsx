f6feb27b43bd8842394d89b1fe76cf5bcdc9e657
john32313 <francois.jonathan13@gmail.com>
2021-01-06 20:04:35 +0100

    small commit for small thing..

--- src/components/SingleRessource.jsx
+++ src/components/SingleRessource.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function SingleRessource(props) {
   const { url, title } = props;
   return (
-    <a href={url} target="_blank" rel="noreferrer">
+    <a href={url} target="_blank" rel="noopener noreferrer">
       {title}
     </a>
   );
